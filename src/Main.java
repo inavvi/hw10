cd2447aa95f719fc70d84b511ae36193b05f1d72
Ivan Votintsev <navvi@mail.ru>
2022-09-20 22:07:53 +0300

    Комит 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Задание 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String whitespace = " ";
+        String fullName = firstName+whitespace+middleName+whitespace+lastName;
+        System.out.println("ФИО сотрудника — "+fullName);
+        System.out.println("Задание 2");
+        String result = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — "+result);
+
+        System.out.println("Задание 3");
+        String bfirstName = "Семён";
+        String bmiddleName = "Семёнович";
+        String blastName = "Иванов";
+        bfirstName = bfirstName.replace("ё", "е");
+        bmiddleName=bmiddleName.replace("ё", "е");
+        String bwhitespace = " ";
+        String bfullName = bfirstName+bwhitespace+bmiddleName+bwhitespace+blastName;
+        System.out.println("ФИО сотрудника — "+bfullName);
     }
 }
